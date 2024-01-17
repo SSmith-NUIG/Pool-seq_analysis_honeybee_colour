da492a426a54fde8bfd37e4adb02fb0050b575d1
SSmith-NUIG <stephensmith@live.ie>
2024-01-17 15:25:12 +0000

    Update gowinda_script.sh

--- gowinda_script.sh
+++ gowinda_script.sh
@@ -6,7 +6,7 @@ java -Xmx2g -jar /home/stephen/gowinda/Gowinda-1.12.jar \
 --annotation-file /home/stephen/gowinda/fixing_input/triple_test/gowinda_gtf3.gtf \
 --gene-set-file /home/stephen/gowinda/fixing_input/triple_test/gowinda_add_HGD2.csv \
 --output-file /home/stephen/gowinda/fixing_input/triple_test/gowinda_snp_results_all_genometesting_fixed.txt \
---simulations 50000 --min-significance 1 \
+--simulations 10000000 --min-significance 1 \
 --gene-definition updownstream2000 \
 --mode snp \
 --detailed-log \
